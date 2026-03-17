7dad026fe6226685665163f3b531b70e6cfdb1f1
IgorMMichels <igor.marcon.michels@gmail.com>
2026-03-17 08:35:52 -0300

    Início atividade

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,26 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, computed } from 'vue'
+const nome = ref('')
+const sobrenome = ref('')
+
+const nomeCompleto = computed(() => {
+  console.log('Calculando nome completo...')
+  return `${nome.value} ${sobrenome.value}`;
+})
+
+const nomeCompleto2 = () => {
+  console.log('Calculando nome completo...')
+  return `${nome.value} ${sobrenome.value}`;
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <input type="text" v-model="nome" />
+    <input type="text" v-model="sobrenome" />
+    <label>Nome completo: {{ nomeCompleto }}</label>
+    <label>Nome completo 2: {{ nomeCompleto2 }}</label>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
